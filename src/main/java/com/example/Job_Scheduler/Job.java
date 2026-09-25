d80c255164dde499b5ed1a5302f19c34cb004f08
dhanush-rao-k <dhanushraokona@gmail.com>
2026-09-26 03:03:04 +0530

    Created Job Class

---
+++ src/main/java/com/example/Job_Scheduler/Job.java
@@ -0,0 +1,116 @@
+package com.example.Job_Scheduler;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name="jobs")
+public class Job {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private JobStatus status;
+
+    @Column(name="run_at", nullable=false)
+    private LocalDateTime runAt;
+
+    @Column(columnDefinition="TEXT")    
+    private String payload;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Job() {
+    }
+
+    public Job(String name, String type, JobStatus status, LocalDateTime runAt, String payload, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.name = name;
+        this.type = type;
+        this.status = status;
+        this.runAt = runAt;
+        this.payload = payload;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public JobStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(JobStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getRunAt() {
+        return runAt;
+    }
+
+    public void setRunAt(LocalDateTime runAt) {
+        this.runAt = runAt;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }   
+    
+}
---
+++ src/test/java/com/example/Job_Scheduler/JobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job_Scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
